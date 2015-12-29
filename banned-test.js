f1824c29aa6e8840113fc4ef05d49fbdafdc347b
Eric Gonzalo <ErSGonzalo@Gmail.com>
2015-12-29 14:16:14 -0500

    Update message and readme clarity

--- banned-test.js
+++ banned-test.js
@@ -4,43 +4,44 @@ var request = require('request');
 
 let stringArr;
 let bannedArray = [];
-var bannedObjectMaker = (uuid, name)=>{
-	return {
-	uuid, //8-4-4-4-12
-    name,
-    "created": "2015-01-01 00:01:01 +0100",
-    "source": "(Unknown)",
-    "expires": "forever",
-    "reason": "Banned by an operator."}
+var bannedObjectMaker = (uuid, name) => {
+    return {
+        uuid, //8-4-4-4-12
+        name,
+        "created": "2015-01-01 00:01:01 +0100",
+        "source": "(Unknown)",
+        "expires": "forever",
+        "reason": "[LL] Banned by an operator."
+    }
 }
 const minecraftRequestURL = 'https://api.mojang.com/users/profiles/minecraft/';
-const altsEmails = 'alts.txt';
 // const umUsernames = './banned folder/333 Unmigrated Usernames.txt';
-const umUsernames = './banned folder/usernames2.txt';
-// const umUsernames = 'testbans.txt';
-fs.readFile(umUsernames, 'utf8', (err, data)=>{
-	stringArr = data.split(/\r?\n/); //carriage return and newline possibly
-	stringArr = stringArr.filter(function(elem, index, self) {
-    	return index == self.indexOf(elem);
-	});
+const umUsernames = './newLists/1033 Premium Minecraft Usernames.txt';
 
-	for(let smallName of stringArr){
-		smallName = smallName.split(':')[0]; //for if unmigrated username with password
-		request.get(minecraftRequestURL+smallName,(error, response, body)=>{
-			try{
-				let smallID = JSON.parse(body);
-				if(smallID.hasOwnProperty('id')){
-					smallID = smallID.id
-					smallID = smallID.substring(0,8) + '-' + smallID.substring(8,12) + '-' + smallID.substring(12,16) + '-' + smallID.substring(16,20) + '-' + smallID.substring(20,32); //format the UUID
-					bannedArray.push(bannedObjectMaker(smallID, smallName));
-					let maxbans = JSON.stringify(bannedArray, null, 2);
-					fs.writeFile('./ban processed/banned-players.json', maxbans,()=>{
-						console.log('Finished writing', smallName);
-					});
-				}
-			}catch(error){
-				console.log(error);
-			}
-		});
-	}
+fs.readFile(umUsernames, 'utf8', (err, data) => {
+    stringArr = data.split(/\r?\n/); //carriage return and newline possibly
+    stringArr = stringArr.filter(function(elem, index, self) {
+        return index == self.indexOf(elem);
+    });
+
+    for (let smallName of stringArr) {
+        // // smallName = smallName.split(':')[0]; //for if unmigrated username with password
+        request.get(minecraftRequestURL + smallName, (error, response, body) => {
+            try {
+                let smallID = JSON.parse(body);
+                // if (smallID.hasOwnProperty('id')) {
+                //     smallID = smallID.id
+                //     smallID = smallID.substring(0, 8) + '-' + smallID.substring(8, 12) + '-' + smallID.substring(12, 16) + '-' + smallID.substring(16, 20) + '-' + smallID.substring(20, 32); //format the UUID
+                //     bannedArray.push(bannedObjectMaker(smallID, smallName));
+                //     let maxbans = JSON.stringify(bannedArray, null, 2);
+                //     fs.writeFile('./processed bans/banned-players.json', maxbans, () => {
+                //         console.log('Finished writing', smallName);
+                //     });
+                // }
+                console.log(smallID);
+            } catch (error) {
+                console.log(error);
+            }
+        });
+    }
 });
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 Minecraft Name Banner
 -----
-An npm script used to created a banned-players.json file to help your server combat spammy alts using leaked lists. 
+An npm script used to created a banned-players.json file to help your server combat spammy alts using leaked lists. Uses Mojang API to get the UUID of the account.
 
 1. Add replace your the string of the variable 'umUsernames' 
 2. node banned-test.js
@@ -8,5 +8,6 @@ An npm script used to created a banned-players.json file to help your server com
 
 ToDos:
 -----
-* Make a GUI for admins
+* Make a GUI for admins, html page likely
 * Easily select file from inner folder
+* Take TooManyRequestsException into consideration when dealing with large lists
